3ccc238a67d9f381c25880fe6a75616c4aade1fd
krishna-prasad16 <krishnaaprasad1609@gmail.com>
2025-01-24 16:26:16 +0530

    latest

--- Project/admin/lib/main.dart
+++ Project/admin/lib/main.dart
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const MaterialApp(debugShowCheckedModeBanner:false, //debug maarum
       home: Login()
     );
   }
